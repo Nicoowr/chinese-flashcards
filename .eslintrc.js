644ce7e90096ca6c6dcf4f6d21405901658a826d
nicoowr <nicolas.li@hotmail.fr>
2024-08-15 17:18:28 +0200

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+
+
+
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
+  {
+    "extends": "next/core-web-vitals"
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+];
